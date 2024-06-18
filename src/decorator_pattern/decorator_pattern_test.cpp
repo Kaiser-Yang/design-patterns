8d65640a816c0fbdc5091fafc90877a9cdf06328
Kaiser-Yang <624626089@qq.com>
2024-06-18 09:27:03 +0800

    Update format

--- src/decorator_pattern/decorator_pattern_test.cpp
+++ src/decorator_pattern/decorator_pattern_test.cpp
@@ -11,20 +11,22 @@
 #include "beverage/house_blend.h"
 
 void DecoratorPatternTest() {
-    std::unique_ptr<Beverage> beverage1 = std::make_unique<Espresso>();
+    std::unique_ptr<Beverage> beverage1, beverage2, beverage3;
+
+    beverage1 = std::make_unique<Espresso>();
     std::cout << beverage1->getDescription() << " $" << beverage1->cost() << std::endl;
 
-    std::unique_ptr<Beverage> beverage2 = std::make_unique<DarkRoast>();
-    beverage2                           = std::make_unique<Mocha>(std::move(beverage2));
-    beverage2                           = std::make_unique<Mocha>(std::move(beverage2));
-    beverage2                           = std::make_unique<Whip>(std::move(beverage2));
+    beverage2 = std::make_unique<DarkRoast>();
+    beverage2 = std::make_unique<Mocha>(std::move(beverage2));
+    beverage2 = std::make_unique<Mocha>(std::move(beverage2));
+    beverage2 = std::make_unique<Whip>(std::move(beverage2));
     std::cout << beverage2->getDescription() << " $" << beverage2->cost() << std::endl;
 
-    std::unique_ptr<Beverage> beverage3 = std::make_unique<HouseBlend>();
-    beverage3                           = std::make_unique<Soy>(std::move(beverage3));
-    beverage3                           = std::make_unique<Mocha>(std::move(beverage3));
-    beverage3                           = std::make_unique<Whip>(std::move(beverage3));
-    beverage3                           = std::make_unique<Milk>(std::move(beverage3));
+    beverage3 = std::make_unique<HouseBlend>();
+    beverage3 = std::make_unique<Soy>(std::move(beverage3));
+    beverage3 = std::make_unique<Mocha>(std::move(beverage3));
+    beverage3 = std::make_unique<Whip>(std::move(beverage3));
+    beverage3 = std::make_unique<Milk>(std::move(beverage3));
     std::cout << beverage3->getDescription() << " $" << beverage3->cost() << std::endl;
 }
 
