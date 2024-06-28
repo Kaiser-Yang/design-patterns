c8ad5f30f825a284df650e35c6917af510d50708
Kaiser-Yang <624626089@qq.com>
2024-06-28 11:17:50 +0800

    Finish the adapter pattern

---
+++ src/adapter_pattern/adapter_pattern_test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+
+#include "adapter/turkey_adpater.h"
+#include "turkey/wild_trukey.h"
+#include "duck/duck.h"
+
+void AdapterPatternTest() {
+    std::shared_ptr<Turkey> turkey = std::make_shared<WildTurkey>();
+    std::shared_ptr<Duck> turkeyAdapter = std::make_shared<TurkeyAdapter>(turkey);
+
+    std::cout << "The Turkey says..." << std::endl;
+    turkey->gobble();
+    turkey->fly();
+
+    std::cout << "The TurkeyAdapter says..." << std::endl;
+    turkeyAdapter->quack();
+    turkeyAdapter->fly();
+}
+
+int main() {
+    AdapterPatternTest();
+    return 0;
+}
