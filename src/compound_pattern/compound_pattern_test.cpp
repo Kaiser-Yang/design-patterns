ef9b254daf7521e7ff5a5cc53cd2e83b2f703a54
Kaiser-Yang <624626089@qq.com>
2024-07-19 21:51:39 +0800

    Apply clang-format

--- src/compound_pattern/compound_pattern_test.cpp
+++ src/compound_pattern/compound_pattern_test.cpp
@@ -1,11 +1,11 @@
 #include <memory>
 
-#include "model/beat_model.h"
-#include "controller/controller_interface.h"
 #include "controller/beat_controller.h"
+#include "controller/controller_interface.h"
+#include "model/beat_model.h"
 
 void CompoundDesignTest() {
-    std::shared_ptr<BeatModelInterface> model = std::make_shared<BeatModel>();
+    std::shared_ptr<BeatModelInterface> model       = std::make_shared<BeatModel>();
     std::shared_ptr<ControllerInterface> controller = std::make_shared<BeatController>(model);
     controller->init();
 }
--- src/multi_pattern/multi_pattern_test.cpp
+++ src/multi_pattern/multi_pattern_test.cpp
@@ -1,12 +1,12 @@
 #include <memory>
 
+#include "composite/flock.h"
 #include "counter/quack_counter.h"
-#include "observer/quackologist.h"
-#include "quackable.h"
 #include "factory/abstract_factory.h"
 #include "factory/counting_duck_factory.h"
-#include "composite/flock.h"
 #include "factory/factory_method.h"
+#include "observer/quackologist.h"
+#include "quackable.h"
 
 size_t QuackCounter::quacks = 0;
 
@@ -15,21 +15,21 @@ void simulate(std::shared_ptr<Quackable> duck) { duck->quack(); }
 void MultiPatternTest() {
     std::unique_ptr<AbstractFactory> factory = std::make_unique<CountingDuckFactory>();
 
-    std::shared_ptr<Flock> flockOfDucks = FactoryMethod::createFlock();
+    std::shared_ptr<Flock> flockOfDucks    = FactoryMethod::createFlock();
     std::shared_ptr<Quackable> redheadDuck = factory->createRedheadDuck();
-    std::shared_ptr<Quackable> duckCall = factory->createDuckCall();
-    std::shared_ptr<Quackable> rubberDuck = factory->createRubberDuck();
-    std::shared_ptr<Quackable> goose = factory->createGooseAdapter();
+    std::shared_ptr<Quackable> duckCall    = factory->createDuckCall();
+    std::shared_ptr<Quackable> rubberDuck  = factory->createRubberDuck();
+    std::shared_ptr<Quackable> goose       = factory->createGooseAdapter();
     flockOfDucks->add(redheadDuck);
     flockOfDucks->add(duckCall);
     flockOfDucks->add(rubberDuck);
     flockOfDucks->add(goose);
 
     std::shared_ptr<Flock> flockOfMallards = FactoryMethod::createFlock();
-    std::shared_ptr<Quackable> mallard1 = factory->createMallardDuck();
-    std::shared_ptr<Quackable> mallard2 = factory->createMallardDuck();
-    std::shared_ptr<Quackable> mallard3 = factory->createMallardDuck();
-    std::shared_ptr<Quackable> mallard4 = factory->createMallardDuck();
+    std::shared_ptr<Quackable> mallard1    = factory->createMallardDuck();
+    std::shared_ptr<Quackable> mallard2    = factory->createMallardDuck();
+    std::shared_ptr<Quackable> mallard3    = factory->createMallardDuck();
+    std::shared_ptr<Quackable> mallard4    = factory->createMallardDuck();
     flockOfMallards->add(mallard1);
     flockOfMallards->add(mallard2);
     flockOfMallards->add(mallard3);
